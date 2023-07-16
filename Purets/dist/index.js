fb4fe71970c6b1e649d059e65ad6abc496030c1b
abrehan2 <rehannajam2@gmail.com>
2023-07-16 20:49:23 +0500

    initial commit

--- Purets/dist/index.js
+++ Purets/dist/index.js
@@ -1,15 +1,29 @@
 "use strict";
 // tsc.cmd --init
 // tsc.cmd -w
+var _User_city;
 console.log("THIS IS REHAN");
 console.log("IM WORKING!!");
 // CLASSES -
+// class User {
+//  public email: string;
+//   name: string;
+//   readonly #city: string = "LA";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//     ;
+//   }
+// }
 class User {
     constructor(email, name) {
-        this.city = "LA";
+        this.email = email;
+        this.name = name;
+        _User_city.set(this, "LA");
         this.email = email;
         this.name = name;
     }
 }
+_User_city = new WeakMap();
 const obj = new User("abc.com", "Rehan");
 // obj.city = "Toronto";
--- Purets/src/index.ts
+++ Purets/src/index.ts
@@ -5,15 +5,27 @@ console.log("THIS IS REHAN");
 console.log("IM WORKING!!");
 
 // CLASSES -
+
+// class User {
+//  public email: string;
+//   name: string;
+//   readonly #city: string = "LA";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//     ;
+//   }
+// }
+
 class User {
-  email: string;
-  name: string;
-  readonly city: string = "LA";
-  constructor(email: string, name: string) {
+  
+  readonly #city: string = "LA";
+  constructor(public email: string, private name: string) {
     this.email = email;
     this.name = name;
   }
 }
+
 
 const obj = new User("abc.com", "Rehan");
 // obj.city = "Toronto";
