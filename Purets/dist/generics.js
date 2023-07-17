f99f37a5f3cf06c321d03744847d7d4b0daefbf5
abrehan2 <rehannajam2@gmail.com>
2023-07-18 01:09:04 +0500

    initial commit

--- Purets/dist/generics.js
+++ Purets/dist/generics.js
@@ -1,2 +1,13 @@
 "use strict";
 // ALLOWS TO REUSE COMPONENTS 
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
--- Purets/src/generics.ts
+++ Purets/src/generics.ts
@@ -1,1 +1,15 @@
 // ALLOWS TO REUSE COMPONENTS 
+const score: Array <number> = []
+const names: Array <string> = []
+
+function identityOne(val: boolean | number): boolean | number {
+    return val;
+}
+
+function identityTwo(val: any): any {
+    return val;
+}
+
+function identityThree<Type>(val: Type): Type {
+    return val;
+}
