a85e639035965e4e38810054ae2a848a2efe1642
abrehan2 <rehannajam2@gmail.com>
2023-07-19 03:09:57 +0500

    initial commit

--- Purets/dist/detection.js
+++ Purets/dist/detection.js
@@ -20,3 +20,43 @@ function isAdmin(account) {
     }
     return false;
 }
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird food";
+    }
+}
+function getTrueShape(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    // return shape.side * shape.side;
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.length * shape.width;
+        default:
+            const _defaultforshape = shape;
+            return _defaultforshape;
+    }
+}
--- Purets/src/detection.ts
+++ Purets/src/detection.ts
@@ -18,6 +18,7 @@ function provideId(id: string | null) {
 }
 
 // IN OPERATOR IS USED TO NARROW DOWN THE TYPES of for interface
+// instanceof checks whether tis object is an instance of some class. It works just like IN operator
 
 interface User {
   myName: string;
@@ -36,6 +37,74 @@ function isAdmin(account: User | Admin) {
   }
 
   return false;
+}
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  } else {
+    pet;
+    return "bird food";
+  }
+}
+
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  side: number;
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+
+  // return shape.side * shape.side;
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+
+    case "square":
+      return shape.side * shape.side;
+
+    case "rectangle":
+      return shape.length * shape.width;
+
+    default:
+      const _defaultforshape: never = shape;
+      return _defaultforshape;
+  }
 }
 
 export {};
